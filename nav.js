f2131e0a9715555f0113fc394f6db73e6110c903
Liam Bradbury <liam.bradbury98@gmail.com>
2021-02-18 17:14:48 +0000

    first commit

---
+++ nav.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById("small-hide");
+let navList = document.getElementById("navList");
+
+
+
+menu.addEventListener("click", function(){
+    navList.classList.toggle("hide");
+    
+    
+})
