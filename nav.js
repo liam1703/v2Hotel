3c73a20c8f00c7c3b816b392c72df625c53e4ee6
Liam Bradbury <liam.bradbury98@gmail.com>
2021-02-20 17:07:53 +0000

    update

--- nav.js
+++ nav.js
@@ -4,7 +4,9 @@ let navList = document.getElementById("navList");
 
 
 menu.addEventListener("click", function(){
+    // navList.style.animation = "0.5s ease";
     navList.classList.toggle("hide");
+    navList.style.animation = "0.5s ease forwards";
     
     
 })
